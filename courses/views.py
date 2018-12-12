2f62180e19faaaa763263dab372a84cc7430d9d6
azatbairamov <3686bairam>
2018-12-12 23:34:53 +0300

    bf

--- courses/views.py
+++ courses/views.py
@@ -6,12 +6,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse('''
-        _...._
-     .'  _ _ `.
-    | ."` ^ `". _,
-    \_;`"---"`|//
-      |       ;/
-      \_     _/
-        `"""`
-        ''')
+    return HttpResponse("Привет! Сейчас сервис запущен и ты можешь написать боту <a "
+                        "href='https://t.me/courses_service_bot'>https://t.me/courses_service_bot</a>")
