fa1f18b884bd8641211820061ce06d5018f25c74
azatbairamov <3686bairam>
2018-12-12 22:48:02 +0300

    bf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,5 +122,3 @@ USE_TZ = True
 STATIC_URL = '/resources/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'resources')
-
-django_heroku.settings(locals())
