22b6ecbec8308762d3d913b6865e8bd291f35600
azatbairamov <3686bairam>
2018-12-19 13:30:08 +0300

    bf

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 
+from bot.main import BotStarter
+
+bot = BotStarter()
+
 
 def index(request):
+    if not bot.is_run():
+        bot.start_bot()
     return HttpResponse(
         "Привет! Можешь написать боту <a href='https://t.me/courses_service_bot'>https://t.me/courses_service_bot</a>")
