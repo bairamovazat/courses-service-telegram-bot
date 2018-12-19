47fbdb9746aef806de5804cc50c780ee88768ab5
azatbairamov <3686bairam>
2018-12-19 09:38:28 +0300

    bf

--- bot/service/TelegramBot.py
+++ bot/service/TelegramBot.py
@@ -15,12 +15,12 @@ class TelegramBot:
         self.bot = telegram.Bot(token=self.token)
         self.updater = Updater(bot=self.bot)
         self.dispatcher = self.updater.dispatcher
-
         self.updater.start_webhook(listen="0.0.0.0",
                                    port=int(os.environ.get('PORT', '5000')),
                                    url_path=token)
 
         self.updater.bot.setWebhook("https://courses-service-telegram-bot.herokuapp.com/" + token)
+
         self.updater.idle()
         # self.init_handler()
 
